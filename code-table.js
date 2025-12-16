b3cf29a3fd017e44b0b59f011683884c3fc9cf66
shabana_asthma <36826017+rupalimo@users.noreply.github.com>
2025-12-16 18:38:06 -0500

    update w content and layout

---
+++ code-table.js
@@ -0,0 +1,39 @@
+var SPREADSHEET_ID_AND_TAB =
+	'1AZTTBz_Rh4KP1yDHaGJsmyysrJOMCMYgWQB6duuzoD4/cookn';
+
+$(document).ready(function () {
+  $.getJSON(
+    'https://opensheet.elk.sh/' + SPREADSHEET_ID_AND_TAB,
+    function (data) {
+      console.log(data);
+
+      data.forEach(function (entry, index) {
+        console.log(entry);
+
+        if (index == 0) return;
+        let header = $(`<h1>` + entry.BlogName + `</h1>`).appendTo('#blog-title');
+
+        let section = $(
+					`<article data-filter-type="` +
+						entry.DishName +
+						`" class="blog--item"><h3>` +
+						entry.DishName +
+						`</h3><span><p class="entry--who">` +
+						entry.Who +
+						`</p><p class="entry--date">` +
+						entry.Date +
+						`</p><p class="entry--tag">Tags: ` +
+						entry.Genre +
+						`</p></span><p class="entry--description">` +
+						entry.Description +
+						`</p><p>` +
+						entry.Notes +
+						`</p>
+				<img src="imgs/` +
+						entry.Image +
+						`" /></article>`
+				).appendTo('#cook-entry');
+      });
+    }
+  );
+})
